05cddff3a072a1f605040480802b12ffaeb6a338
GolovinDen <denisgolovin99@outlook.com>
2024-11-28 14:42:08 +0300

    lesson-03/01 complete

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,7 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
-
-function calculateAverage() {}
+const number_1 = 5
+const number_2 = 6
+function calculateAverage(number_1, number_2) {
+    let result = (number_1 + number_2) / 2
+    console.log(result);
+}
