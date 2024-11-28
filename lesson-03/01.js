681a3bbfdee2060285bdc66f36d82675f7604430
GolovinDen <denisgolovin99@outlook.com>
2024-11-28 15:00:51 +0300

    less03/01

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,7 +1,5 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
-let a = 10
-let b = 20
+
 function calculateAverage(a, b) {
     const result = (a + b) / 2
 }
-console.log(result)
