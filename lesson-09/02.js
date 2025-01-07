d336d1acab4084fb4c4c251c2d53ee7f0cfa414b
GolovinDen <denisgolovin99@outlook.com>
2025-01-07 17:45:06 +0300

    lesson-09-02

--- lesson-09/02.js
+++ lesson-09/02.js
@@ -24,4 +24,22 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = () => {}
+const map = (array, callback) => {
+  const newArray = []
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    const index = i
+    
+      newArray.push(callback(element, index))
+  
+  }
+  return newArray
+}
+
+// const numbers = [1, 2, 3, 4, 5]
+
+// const doubledNumbers = map(numbers, (element, index) => {
+//   return element * 2
+// })
+
+// console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
